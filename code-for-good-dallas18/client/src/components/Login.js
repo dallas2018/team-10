b2ac813375c75945ba8e834699cd36a438aa0728
EvanShui <evanshui0@gmail.com>
2018-10-05 21:49:35 -0500

    created fields

--- code-for-good-dallas18/client/src/components/Login.js
+++ code-for-good-dallas18/client/src/components/Login.js
@@ -15,6 +15,19 @@ class Login extends Component {
                 <div>
                     <p>what</p>
                 </div>
+                <form>
+                    <label>
+                        Name:
+                        <input type="text" name="name"/>
+                    </label>
+                    <br></br>
+                    <label>
+                        Password:
+                        <input type="text" name="password"/>
+                    </label>
+                    <br></br>
+                    <input type="submit"></input>
+                </form>
             </div>
         );
     }
