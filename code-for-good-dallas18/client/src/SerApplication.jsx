948a1bf6bad66996a09697ba610275b39b44fdb0
Selynna Sun <ssun098@gmail.com>
2018-10-06 02:33:09 -0500

    Add firebase

--- code-for-good-dallas18/client/src/SerApplication.jsx
+++ code-for-good-dallas18/client/src/SerApplication.jsx
@@ -4,8 +4,19 @@ import fire from './fire';
 import InputFieldGroup from './InputFieldGroup.jsx';
 import InputSection from './InputSection.jsx';
 import styles from './SerApplication.module.css';
+import firebase from './fire.js';
 
 class SerApplication extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   state = {
     response: ''
   };
@@ -24,11 +35,39 @@ class SerApplication extends Component {
 
     return body;
   };
+  
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+		e.preventDefault();
+  	const itemsRef = firebase.database().ref('users');
+  	const item = {
+  	  firstName: this.state.firstName,
+  	  lastName: this.state.lastName
+  	}
+  	itemsRef.push(item);
+  	this.setState({
+  	  firstName: '',
+  	  lastName: ''
+  	});
+  }
 
   render() {
     return (
       <div className="App">
-        <InputSection type="text" required="false">Hello</InputSection>
+        <form onSubmit={this.handleSubmit}>
+          <span>First Name</span>
+          <input type="text" name="firstName" onChange={this.handleChange} value={this.state.firstName}/>
+          <br/>
+          <span>Last Name</span>
+          <input type="text" name="lastName" onChange={this.handleChange} value={this.state.lastName}/>
+          <br/>
+          <button>Submit</button>
+        </form>
         
         <p className="App-intro">{this.state.response}</p>
       </div>
