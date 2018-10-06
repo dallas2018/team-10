20ff8860daffd3e076d4446b9c91e6d4b6be8837
EvanShui <evanshui0@gmail.com>
2018-10-06 04:26:07 -0700

    creat_react supports text type questions

--- scripts/create_react.py
+++ scripts/create_react.py
@@ -59,15 +59,13 @@ code = """
 
 """
 
-ret = """
+ret_top = """
   render() {
     return (
       <div className="App">
           <form onSubmit={this.handleSubmit}>
-            <InputSection name="Demographic Information">
-              <InputFieldGroup type="text" name="firstName" onChange={this.handleChange} value={this.state.firstName}>First Name</InputFieldGroup>
-              <InputFieldGroup type="text" name="lastName" onChange={this.handleChange} value={this.state.lastName}>Last Name</InputFieldGroup>
-            </InputSection>
+"""
+ret_bot = """
             <button>Submit</button>
           </form>
         <p className="App-intro">{this.state.response}</p>
@@ -77,24 +75,59 @@ ret = """
 }
 
 export default SerApplication;
+
+"""
+ret = """
+            <InputSection name="Demographic Information">
+              <InputFieldGroup type="text" name="firstName" onChange={this.handleChange} value={this.state.firstName}>First Name</InputFieldGroup>
+              <InputFieldGroup type="text" name="lastName" onChange={this.handleChange} value={this.state.lastName}>Last Name</InputFieldGroup>
+            </InputSection>
 """
 
 input_field_section = "            <InputSection name=\"{}\">"
 input_field_section_end = "            </InputSection>"
 
-question = "              <InputFieldGroup type=\"{question_type}\" name=\"firstName\" onChange={{this.handleChange}} value={{this.state.firstName}}>{ques}</InputFieldGroup>"
+question = "              <InputFieldGroup type=\"{question_type}\" name=\"{var_name}\" onChange={{this.handleChange}} value={{this.state.{var_name}}}>{ques}</InputFieldGroup>"
 
-with open('question.json', 'r') as f:
+with open('question_v2.json', 'r') as f:
     x = json.load(f)
 
-print(ret)
-print(input_field_section)
-print(x)
+state_lst = []
+
+for section, questions in x.items():
+    # print(input_field_section.format(section))
+    for question_obj in questions:
+        if question_obj['var_name'] != 'hello':
+            '''
+            print(question.format(question_type=question_obj['type'],
+                ques=question_obj['question'],
+                var_name=question_obj['var_name']))
+            '''
+            state_lst.append(question_obj['var_name'] + ': \'\',')
+            # print(question_obj['question'])
+            # print(question_obj['answer'])
+            # print(question_obj['type'])
+            # print(question_obj['var_name'])
+    # print(input_field_section_end)
+state_lst[-1] = state_lst[-1][0:-1]
+state = ('\n'.join(state_lst))
+
+print(constructor_top)
+print(state)
+print(constructor_bot)
+print(code)
+print(ret_top)
 for section, questions in x.items():
     print(input_field_section.format(section))
     for question_obj in questions:
-        print(question.format(question_type=question_obj['type'], ques=question_obj['question']))
-        print(question_obj['question'])
-        print(question_obj['answer'])
-        print(question_obj['type'])
+        if question_obj['var_name'] != 'hello':
+            print(question.format(question_type=question_obj['type'],
+                ques=question_obj['question'],
+                var_name=question_obj['var_name']))
+            state_lst.append(question_obj['var_name'] + ': \'\',')
+            # print(question_obj['question'])
+            # print(question_obj['answer'])
+            # print(question_obj['type'])
+            # print(question_obj['var_name'])
     print(input_field_section_end)
+print(ret_bot)
