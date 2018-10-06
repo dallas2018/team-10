0a305a8de96b96d41a7b341fd4fb0e75a4bfc54a
EvanShui <evanshui0@gmail.com>
2018-10-05 22:54:34 -0700

    found section title

--- scripts/scrape_questions.py
+++ scripts/scrape_questions.py
@@ -1,1 +1,13 @@
-print('hello')
+import urllib.request
+import re
+from bs4 import BeautifulSoup as bf
+from urllib.request import Request, urlopen, build_opener
+
+base_url = 'https://www.tfaforms.com/429640'
+
+opener = build_opener()
+page = opener.open(base_url)
+soup = bf(page, 'lxml')
+for i in (soup.findAll('fieldset', {'class': 'section column'})):
+    print(i.select('legend')[0].text)
+    print('--------------------------------------')
