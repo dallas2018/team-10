5342ab498026e2e671f482599c7277d7a4a1f420
EvanShui <evanshui0@gmail.com>
2018-10-06 00:16:20 -0700

    finished scraping

--- scripts/scrape_questions.py
+++ scripts/scrape_questions.py
@@ -5,6 +5,10 @@ from urllib.request import Request, urlopen, build_opener
 
 base_url = 'https://www.tfaforms.com/429640'
 
+dct = {}
+temp_question = ''
+temp_choice = []
+
 opener = build_opener()
 page = opener.open(base_url)
 soup = bf(page, 'lxml')
@@ -13,17 +17,23 @@ for i in (soup.findAll('fieldset', {'class': 'section column'})):
     print('--------------------------------------')
     # getting options
     # sub-categories
-    print(i.prettify())
+    #print(i.prettify())
     if i.fieldset:
         print('sub section title: ', i.fieldset.legend)
     for j in (i.findAll('label', {'class': 'label'})):
         if not j.span:
             print("question: ", j.text)
+            temp_question = j.text
+            dct[temp_question] = []
         else:
             print('choice: ', j.text)
+            dct[temp_question].append(j.text)
         if j.find_next_siblings('div') != []:
-            print("option div: ", j.find_next_siblings('div')[0].prettify())
+            # print("option div: ", j.find_next_siblings('div')[0].prettify())
             options = j.find_next_siblings('div')[0].findAll('option')
             if options != []:
-                print("dropdown: ", options[0].text)
-        print('--------------------------------------')
+                # print("dropdown: ", options[0].text)
+                for x in options:
+                    dct[temp_question].append(x.text)
+                    print('choice: ', x.text)
+print(dct)
